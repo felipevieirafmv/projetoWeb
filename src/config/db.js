53ae03c6c76441fd0308ca29112665f0eed142a3
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2023-07-13 08:15:34 -0300

    front att

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const database = new sequelize('projetoWeb', 'VieiraTavares', 'etstech31415',
 {
     dialect: 'mssql', 
     host:'localhost', 
-    port: 50181,
+    port: 61654,
     timezone: '-03:00'
 });
 
