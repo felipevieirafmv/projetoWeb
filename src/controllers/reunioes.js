b0c7fc71f3a62a2e99d3de2ce440709e1136c06d
Felipe Vieira <felipevieirafmv@gmail.com>
2023-07-17 13:18:00 -0300

    minhas reuniões começado

--- src/controllers/reunioes.js
+++ src/controllers/reunioes.js
@@ -1,8 +1,54 @@
+const sala = require('../model/sala');
+const pessoa = require('../model/pessoa');
+const pessoaReuniao = require('../model/pessoaReuniao');
+const reuniao = require('../model/reuniao');
+const { Op } = require("sequelize");
+
 module.exports = {
     async reunioesGet(req, res){
         if(!req.session.usuario){
-            res.redirect('/')
+            return res.redirect('/')
         }
-        res.render('../views/reunioes');
+
+        var hoje = new Date(Date.now());
+
+        console.log(hoje);
+
+        const reunioes = await pessoaReuniao.findAll({
+            raw: true,
+            attributes: ['Responsavel'],
+            include: [
+            {
+                model: reuniao,
+                attributes: ['Assunto', 'HorarioInicio', 'HorarioFim', 'IdReuniao', 'IDSala', 'Observacoes'],
+                include: [{
+                    model: sala,
+                    attributes: ['Nome'],
+                }],
+                where: {
+                    HorarioInicio:{[Op.gte]: hoje}
+                }
+            }
+            ],
+            where: {Usuario: req.session.usuario}
+        });
+
+        const dataComIntl = new Intl.DateTimeFormat('pt-BR', {
+            dateStyle: 'short',
+            timeStyle: 'medium',
+        });
+
+        var datasI = []
+        var datasF = []
+
+        for(let i=0; i<reunioes.length; i++){
+            datasI.push(dataComIntl.format(reunioes[i].HorarioInicio))
+            datasF.push(dataComIntl.format(reunioes[i].HorarioFim))
+        }
+
+        console.log(datasI)
+        console.log(datasF)
+
+        res.render('../views/reunioes', {reunioes, datasI, datasF});
     }
 }
